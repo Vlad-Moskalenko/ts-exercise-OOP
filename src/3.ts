78b330347539207195745687b68f8e2aa3d56eac
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-08-05 11:39:29 +0300

    done ts oop

--- src/3.ts
+++ src/3.ts
@@ -2,12 +2,22 @@
   Ви створюєте гру, де є персонажі з різними ролями.
   Зараз ви працюєте над класом Wizard, який має реалізовувати два інтерфейси - ICharacter та ISpellCaster.
 
-  Визначте інтерфейси ICharacter та ISpellCaster так, щоб вони відповідали вимогам класу Wizard. 
-  Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp. 
+  Визначте інтерфейси ICharacter та ISpellCaster так, щоб вони відповідали вимогам класу Wizard.
+  Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp.
   Інтерфейс ISpellCaster повинен включати метод castSpell.
 */
 
 // реалізація класу Wizard
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+  levelUp(): void;
+}
+
+interface ISpellCaster {
+  levelUp(): void
+}
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
     if (this.level < 1) {
@@ -15,15 +25,15 @@ class Wizard implements ICharacter, ISpellCaster {
     }
   }
 
-  introduce(phrase: string): void {
+  introduce(phrase: string) {
     console.log(phrase + ', ' + this.name);
   }
 
-  castSpell(): void {
+  castSpell() {
     console.log('Casting a spell, behold my power!');
   }
 
-  levelUp(): void {
+  levelUp() {
     this.level++;
     console.log(`Level up! New level is ${this.level}`);
   }
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,61 @@
+
+class Key {
+  private signature:number
+
+  constructor() {
+    this.signature = Math.random()
+  }
+
+  getSignature(): number {
+    return this.signature
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+  }
+
+  getKey():number {
+    return this.key.getSignature()
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key){
+    this.key = key
+  }
+
+  comeIn(person: Person):void {
+     if (this.door) {
+      this.tenants.push(person);
+      console.log('Person came into the house.');
+    } else {
+      console.log('The door is closed. Cannot come in.');
+    }
+  }
+
+  abstract openDoor(key: number):void
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key:number): void {
+    if(key === this.key.getSignature()){
+      this.door = true;
+      console.log('The door is opened.');
+    } else {
+      console.log('The key does not match. Cannot open the door.');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
